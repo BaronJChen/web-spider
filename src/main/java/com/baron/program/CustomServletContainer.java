c4fc86f20b46fc7ac7f7d32cc3a48164ceecca1d
Baron.Chen <baronchen@augmentum.com.cn>
2017-06-03 18:45:15 +0800

    push to git

---
+++ src/main/java/com/baron/program/CustomServletContainer.java
@@ -0,0 +1,16 @@
+package com.baron.program;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Baron.Chen on 2017/6/3.
+ */
+@Component
+public class CustomServletContainer implements EmbeddedServletContainerCustomizer {
+    public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+        configurableEmbeddedServletContainer.setPort(80);
+        configurableEmbeddedServletContainer.setContextPath("");
+    }
+}
