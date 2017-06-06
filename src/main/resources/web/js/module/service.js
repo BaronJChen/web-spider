ff3f85572fe4361d8f91b4a0ea5c937fa5ba4815
Baron.Chen <baronchen@augmentum.com.cn>
2017-06-06 09:58:07 +0800

    add jrebel

--- src/main/resources/web/js/module/service.js
+++ src/main/resources/web/js/module/service.js
@@ -1,25 +1,27 @@
 /**
  * Created by Jason on 2017/6/4.
  */
-define(['angular', 'restangular'], function(angular) {
+define(['angular', 'restangular'], function (angular) {
     var service = angular.module('service', ['restangular'], angular.noop);
-    
+
     service.config([
         'RestangularProvider',
-        function(RestangularProvider) {
-             RestangularProvider.setErrorInterceptor(function(response, deferred, responseHandler) {
-                 switch (response.status) {
-                     case 403:
-                         console.log(403);
-                     case 500:
-                         console.log(500);
-                     case 404:
-                         console.log(404);
-                     default:
-                         console.log('unknown error: ' + response);
-                 }
-             });
+        function (RestangularProvider) {
+            RestangularProvider.setBaseUrl('/api');
+            RestangularProvider.setErrorInterceptor(function (response, deferred, responseHandler) {
+                switch (response.status) {
+                    case 403:
+                        console.log(403);
+                    case 500:
+                        console.log(500);
+                    case 404:
+                        console.log(404);
+                    default:
+                        console.log('unknown error: ' + response);
+                }
+            });
         }
-      ]);
+    ]);
+
     return service;
 });
