014d73c7e4205b535750593ddac4efe6f4c0ae3f
Baron.Chen <baronchen@augmentum.com.cn>
2017-06-06 09:30:29 +0800

    fix rest angular inject bug and add banner

--- src/main/resources/web/js/module/service.js
+++ src/main/resources/web/js/module/service.js
@@ -3,22 +3,23 @@
  */
 define(['angular', 'restangular'], function(angular) {
     var service = angular.module('service', ['restangular'], angular.noop);
-    service.run([
+    
+    service.config([
         'RestangularProvider',
         function(RestangularProvider) {
-            // RestangularProvider.setErrorInterceptor(function(response, deferred, responseHandler) {
-            //     switch (response.status) {
-            //         case 403:
-            //             console.log(403);
-            //         case 500:
-            //             console.log(500);
-            //         case 404:
-            //             console.log(404);
-            //         default:
-            //             console.log('unknown error: ' + response);
-            //     }
-            // });
+             RestangularProvider.setErrorInterceptor(function(response, deferred, responseHandler) {
+                 switch (response.status) {
+                     case 403:
+                         console.log(403);
+                     case 500:
+                         console.log(500);
+                     case 404:
+                         console.log(404);
+                     default:
+                         console.log('unknown error: ' + response);
+                 }
+             });
         }
-    ]);
+      ]);
     return service;
 });
